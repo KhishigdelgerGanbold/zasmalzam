c403ab9aa45930b08859a7aae59f84f75d051bc0
KhishigdelgerGanbold <weskera428@gmail.com>
2023-11-30 14:57:59 +0800

    revert

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var loader = document.getElementById("preloader");
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-})
+// var loader = document.getElementById("preloader");
+// window.addEventListener("load", function(){
+//     loader.style.display = "none";
+// })
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
